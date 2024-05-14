f2f763363b1d90d8b7c374d34e158d67a225c429
avoitiuk <anna.voitiuk01@currys.co.uk>
2024-05-14 11:32:49 +0300

    added log4j2 config file and env logging; fixed switching envs

--- src/main/java/org/github/bubochkana/config/ServiceConfigLoader.java
+++ src/main/java/org/github/bubochkana/config/ServiceConfigLoader.java
@@ -1,35 +1,33 @@
 package org.github.bubochkana.config;
 
 import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.github.bubochkana.config.models.ServiceConfigDto;
 
 public class ServiceConfigLoader {
+  private static final Logger LOGGER = LogManager.getLogger();
 
   /**
    * Method to set ServiceConfigDto model with maven properties
    *
    * @return ServiceConfigDto
    */
-//  public ServiceConfigDto loadServiceConfig() {
-//    Properties mavenProperties = MavenProperties.getMavenProperties();
-//    return new ServiceConfigDto()
-//        .apiUrl(mavenProperties.getProperty("dev.placeholder.service.url"))
-//        .apiPort(Integer.parseInt(mavenProperties.getProperty("dev.placeholder.service.port")))
-//        .apiBasePath(mavenProperties.getProperty("dev.placeholder.service.basepath"));
-//  }
-
   public ServiceConfigDto loadServiceConfig() {
     Properties mavenProperties = MavenProperties.getMavenProperties();
     ServiceConfigDto serviceConfigDto = new ServiceConfigDto();
 
+    String executionEnvironment = mavenProperties.getProperty("execution.environment");
+    LOGGER.info("The tests are running on {} environment", executionEnvironment);
+
     for (String key : mavenProperties.stringPropertyNames()) {
-      if (key.contains("dev.placeholder.service.url")) {
+      if (key.contains(executionEnvironment + ".placeholder.service.url")) {
         serviceConfigDto.apiUrl(mavenProperties.getProperty(key));
       }
-      if (key.contains("dev.placeholder.service.port")) {
+      if (key.contains(executionEnvironment + ".placeholder.service.port")) {
         serviceConfigDto.apiPort(Integer.parseInt(mavenProperties.getProperty(key)));
       }
-      if (key.contains("dev.placeholder.service.basepath")) {
+      if (key.contains(executionEnvironment + ".placeholder.service.basepath")) {
         serviceConfigDto.apiBasePath(mavenProperties.getProperty(key));
       }
     }
